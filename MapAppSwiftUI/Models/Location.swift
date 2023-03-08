9a9544407ffa7f523f1b1c239e40368d58b2754c
DenysProg <denysnikolaichuk@gmail.com>
2023-03-08 14:10:51 +0200

    Set up MVVM Assets Data Model

---
+++ MapAppSwiftUI/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapAppSwiftUI
+//
+//  Created by Denys Nikolaichuk on 08.03.2023.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
