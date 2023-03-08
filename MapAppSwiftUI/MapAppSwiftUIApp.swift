08cfa4910c960bf4f2ab63342b29fb63394b66f1
DenysProg <denysnikolaichuk@gmail.com>
2023-03-08 13:09:54 +0200

    Initial Commit

---
+++ MapAppSwiftUI/MapAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppSwiftUIApp.swift
+//  MapAppSwiftUI
+//
+//  Created by Denys Nikolaichuk on 08.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
